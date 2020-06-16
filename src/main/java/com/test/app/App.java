432f8acb4ff4f534506fc0ead8f984e82afcaad7
Your Name <you@example.com>
2020-06-16 17:41:07 +0530

    added constant class and change in app.java

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -1,17 +1,14 @@
 package com.test.app;
 
-
-
 public class App {
 	
 	public void addData(String data){
-		if(data.equals("TEST")) {
+		if(data.equals(Constant.DATA_TYPE)) {
 			System.out.println("Welcome");
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/main/java/com/test/app/Constant.java
@@ -0,0 +1,7 @@
+package com.test.app;
+
+public class Constant {
+	
+	public static String DATA_TYPE = "TEST";
+
+}
