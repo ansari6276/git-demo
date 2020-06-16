1f2a5dbbfc95a75cd3d1762e37876acd651212e4
Zahir <ansari01151994@gmail.com>
2020-06-16 14:59:32 +0530

    added git-demo project to local repository

---
+++ src/main/java/com/test/app/App.java
@@ -0,0 +1,14 @@
+package com.test.app;
+
+public class App {
+	
+	public void addData() {
+		System.out.println("Add Data() Method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
